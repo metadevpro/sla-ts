0c7eb9bb54961cb8980475c517348476f790aba0
Pedro J. Molina <pjmolina@gmail.com>
2022-05-13 20:27:03 +0200

    fix lint

--- src/cli/commands/document.ts
+++ src/cli/commands/document.ts
@@ -1,6 +1,6 @@
 import { Command } from '../command';
 
 export const documentSla = (cmd: Command): number => {
-  console.log('document: not implemented yet');
+  console.log('document: not implemented yet' + cmd.type);
   return 0;
 };
