65d79b6e5e0f871a0fa4ee6b6ebc0984c5b50d4f
Pedro J. Molina <pjmolina@gmail.com>
2022-05-20 10:17:28 +0200

    improved doc generation for limits

--- src/cli/commands/document.ts
+++ src/cli/commands/document.ts
@@ -101,9 +101,9 @@ const generateProperty = (
   value: string,
   prefix: string
 ): string => {
-  return `${prefix}<div class="${fieldName}">
+  return `${prefix}<div class="${fieldName} field">
 ${prefix}  <label for="${fieldName}">${label}</label>
-${prefix}  <span name="${fieldName}">${value}</span>
+${prefix}  <div name="${fieldName}">${value}</div>
 ${prefix}</div>`;
 };
 
@@ -206,7 +206,12 @@ const generateRates = (planName: string, plan: PlanObject): string => {
 
   const content = !rates
     ? '<span>No rates were defined.</span>'
-    : Object.keys(rates).map((path) => generateRate(path, rates[path]));
+    : tableForRatesOrQuota(
+        'rates',
+        Object.keys(rates)
+          .map((path) => generateRate(path, rates[path]))
+          .join('\n')
+      );
 
   const html = `<div class="rates">
     <a name="${planName}-rates">
@@ -221,7 +226,12 @@ const generateQuotas = (planName: string, plan: PlanObject): string => {
 
   const content = !quotas
     ? '<span>No quotas were defined.</span>'
-    : Object.keys(quotas).map((key) => generateQuota(key, quotas[key]));
+    : tableForRatesOrQuota(
+        'quotas',
+        Object.keys(quotas)
+          .map((path) => generateRate(path, quotas[path]))
+          .join('\n')
+      );
 
   const html = `<div class="quotas">
     <a name="${planName}-quotas">
@@ -232,47 +242,42 @@ const generateQuotas = (planName: string, plan: PlanObject): string => {
   return html;
 };
 
-const generateRate = (path: string, rate: PathObject): string => {
-  return generateRateOrQuota('rate', path, rate);
-};
-const generateQuota = (path: string, quota: PathObject): string => {
-  return generateRateOrQuota('quota', path, quota);
+const tableForRatesOrQuota = (type: string, contentHtml: string): string => {
+  return `<table class="${type}">
+  <thead>
+    <th class="verb">Verb</th>
+    <th class="path">Path</th>
+    <th class="metric">Metric</th>
+    <th class="limits">Limits</th>
+  </thead>
+  <tbody>
+    ${contentHtml}
+  </tbody>
+</table>`;
 };
 
-const generateRateOrQuota = (type: string, path: string, po: PathObject): string => {
-  const limits = generateLimitsForPath(path, po);
-  return `<table class="${type}">
-    <thead>
-      <th class="verb">Verb</th>
-      <th class="path">Path</th>
-      <th class="metric">Metric</th>
-      <th class="limits">Limits</th>
-    </thead>
-    <tbody>
-      ${limits}
-      <tr>
-        <td class="verb"></td>
-        <td class="path"></td>
-        <td class="metric"></td>
-        <td class="limit"></td>
-      </tr>
-    </tbody>
-  </table>`;
+const generateRate = (path: string, rate: PathObject): string => {
+  return generateLimitsForPath(path, rate);
+};
+const generateQuota = (path: string, quota: PathObject): string => {
+  return generateLimitsForPath(path, quota);
 };
 
 const generateLimitsForPath = (path: string, pathObject: PathObject): string => {
   const poLimits: { verb: string; path: string; metric: string; limits: LimitObject[] }[] = [];
 
+  let firstTime = true;
   Object.keys(pathObject).map((verb) => {
     const operationalObject = pathObject[verb];
     Object.keys(operationalObject).map((metric) => {
       const limits = operationalObject[metric] || [];
       poLimits.push({
-        verb,
-        path,
+        verb: firstTime ? verb : '',
+        path: firstTime ? path : '',
         metric,
         limits
       });
+      firstTime = false;
     });
   });
 
